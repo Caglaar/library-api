830994824ed7f996489ba0710e1b99823d9c24b9
Muhammed Emin caglar <em1nc4glar@gmail.com>
2024-12-04 00:59:25 +0300

    added postgresql connection and tables

--- src/main/java/com/backend/model/Book.java
+++ src/main/java/com/backend/model/Book.java
@@ -6,15 +6,38 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.util.*;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "book")
 public class Book {
-    private String id;
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "isbn")
     private String isbn;
-    private String name;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "author")
     private String author;
+    @Column(name = "published_date")
     private Date published_date;
+    @Column(name = "is_available")
     private Boolean is_available;
+    
+    @ManyToOne
+    @JoinColumn(name = "owner_id", referencedColumnName = "id")
+    private Member owner;
 }
--- src/main/java/com/backend/model/Member.java
+++ src/main/java/com/backend/model/Member.java
@@ -2,8 +2,19 @@ package com.backend.model;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,13 +25,28 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User implements UserDetails{
-    private String id;
-    private String firstname;
-    private String lastname;
+@Entity
+@Table(name = "member")
+public class Member implements UserDetails{
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "first_name")
+    private String first_name;
+    @Column(name = "last_name")
+    private String last_name;
+    @Column(name = "mail")
     private String mail;
+    @Column(name = "password", nullable = false)
     private String password;
+    @Column(name = "username", nullable = false)
     private String username;
+    @Column(name = "is_admin")
+    private Boolean is_admin;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private List<Book> books;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
